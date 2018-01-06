10a1b01fd1c895a550ef93bd88ea8caf25a37f5c
Wusch <florian.otto@mindbird.de>
2018-01-06 03:54:54 +0100

    Add missing germans translation

--- languages/de/tl_calendar.php
+++ languages/de/tl_calendar.php
@@ -12,6 +12,10 @@
 /**
  * Fields
  */
+$GLOBALS['TL_LANG']['tl_calendar']['subscription_enable']       = [
+    'Anmeldungen aktivieren',
+    'Nutzern erlauben sich für Termine anzumelden.',
+];
 $GLOBALS['TL_LANG']['tl_calendar']['subscription_reminders'] = [
     'Erinnerungmails aktivieren',
     'Aktiviert Erinnerungs-E-Mails für im Kalender eingetragene Anlässe.',
@@ -21,11 +25,15 @@ $GLOBALS['TL_LANG']['tl_calendar']['subscription_time']      = [
     'Bitte geben Sie die ungefähre Zeit ein, wann die Erinnerung verschickt werden sollte.',
 ];
 $GLOBALS['TL_LANG']['tl_calendar']['subscription_days']      = [
-    'Tage vor dem Anlass',
+    'Tage vor dem Termin',
     'Bitte geben Sie in einer Komma getrennten Liste an, wieviele Tage vor dem Anlass Erinnerungsmails verschickt werden sollten.',
+];
+$GLOBALS['TL_LANG']['tl_calendar']['subscription_notification'] = [
+    'Erinnerungsbenachrichtigung',
+    'Bitte wählen Sie die Benachrichtung, welche als Erinnerung versendet werden soll, aus.',
 ];
 
 /**
- * Fields
+ * Legends
  */
 $GLOBALS['TL_LANG']['tl_calendar']['subscription_legend'] = 'Anmelde-Einstellungen';
