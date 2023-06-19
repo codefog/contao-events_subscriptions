1a2c571e6f1a692c8cd268d9c03d004ef5e0f188
Kamil Kuzminski <kamil.kuzminski@codefog.pl>
2023-06-19 11:59:44 +0200

    Fix PHP 8 error

--- dca/tl_calendar_events_subscription.php
+++ dca/tl_calendar_events_subscription.php
@@ -83,7 +83,7 @@ $GLOBALS['TL_DCA']['tl_calendar_events_subscription'] = [
                 'label'      => &$GLOBALS['TL_LANG']['tl_calendar_events_subscription']['delete'],
                 'href'       => 'act=delete',
                 'icon'       => 'delete.gif',
-                'attributes' => 'onclick="if(!confirm(\''.$GLOBALS['TL_LANG']['MSC']['deleteConfirm'].'\'))return false;Backend.getScrollOffset()"',
+                'attributes' => 'onclick="if(!confirm(\''.($GLOBALS['TL_LANG']['MSC']['deleteConfirm'] ?? '').'\'))return false;Backend.getScrollOffset()"',
             ],
             'show'   => [
                 'label' => &$GLOBALS['TL_LANG']['tl_calendar_events_subscription']['show'],
